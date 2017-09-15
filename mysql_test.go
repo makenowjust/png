23d484e7d069c62b0056e80dcf64fb109b16af06
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-09-15 18:53:10 +0900

    Add real MySQL test

--- mysql_test.go
+++ mysql_test.go
@@ -3,22 +3,101 @@ package png
 import (
 	"testing"
 
+	"context"
+	"io/ioutil"
 	"net/url"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"strings"
 )
 
-func TestMySQLURLToDSN(t *testing.T) {
-	u, err := url.Parse("mysql://localhost")
+type MySQLServer struct {
+	Close func() error
+}
+
+func runMySQLServer(mysqldPath, port string) (mysqld *MySQLServer, err error) {
+	datadir, err := ioutil.TempDir("", "mysqld-datadir")
 	if err != nil {
-		panic(err)
+		return
 	}
 
-	p := &MySQLPinger{url: u}
-
-	if dsn := p.urlToDSN(); dsn != "tcp(localhost)" {
-		t.Fatalf("unexpected dsn: %#v", dsn)
+	err = exec.Command(mysqldPath, "--initialize-insecure", "--datadir="+datadir, "--log-error=mysqld.err").Run()
+	if err != nil {
+		os.RemoveAll(datadir)
+		return
 	}
 
-	if dsn := p.urlToDSN(); dsn != "tcp(localhost)" {
-		t.Fatalf("unexpected dsn: %#v", dsn)
+	err = exec.Command(mysqldPath, "--daemonize", "--datadir="+datadir, "--log-error=mysqld.err", "--port="+port, "--pid-file=mysqld.pid").Run()
+	if err != nil {
+		return
 	}
+
+	mysqld = &MySQLServer{
+		Close: func() error {
+			defer os.RemoveAll(datadir)
+
+			pidFile, err := ioutil.ReadFile(path.Join(datadir, "mysqld.pid"))
+			if err != nil {
+				return err
+			}
+
+			pid, err := strconv.Atoi(strings.TrimSpace(string(pidFile)))
+			if err != nil {
+				return err
+			}
+
+			process, err := os.FindProcess(pid)
+			if err != nil {
+				return err
+			}
+
+			return process.Kill()
+		},
+	}
+	return
+}
+
+func TestMySQLPinger(t *testing.T) {
+	mysqldPath, err := exec.LookPath("mysqld")
+	if err != nil {
+		t.Skip("mysqld is not found")
+	}
+
+	mysqld, err := runMySQLServer(mysqldPath, "13306")
+	if err != nil {
+		t.Fatalf("failed in starting mysqld: %+#v", err)
+	}
+	defer mysqld.Close()
+
+	t.Run("OK", func(t *testing.T) {
+		u, err := url.Parse("mysql://root:@localhost:13306/mysql")
+		if err != nil {
+			panic(err)
+		}
+
+		p := &MySQLPinger{url: u}
+		err = p.Ping(context.Background())
+		if err != nil {
+			t.Fatalf("failed in p.Ping(): %+#v", err)
+		}
+	})
+
+	t.Run("Fail", func(t *testing.T) {
+		u, err := url.Parse("mysql://root:@localhost:13306/not_found")
+		if err != nil {
+			panic(err)
+		}
+
+		p := &MySQLPinger{url: u}
+		err = p.Ping(context.Background())
+		if err == nil {
+			t.Fatal("succeeded in p.Ping()")
+		}
+
+		if msg := err.Error(); !strings.HasPrefix(msg, "failed in MySQL ping: ") {
+			t.Fatalf("unexpected error message: %#v", msg)
+		}
+	})
 }
